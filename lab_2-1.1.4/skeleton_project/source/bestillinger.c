e6440e5a703f5c6f37a075b5f2085bc0df148533
Simennha <simennha@stud.ntnu.no>
2026-02-23 14:05:04 +0100

    kkkb

--- lab_2-1.1.4/skeleton_project/source/bestillinger.c
+++ lab_2-1.1.4/skeleton_project/source/bestillinger.c
@@ -11,8 +11,11 @@
 
 #include "bestillinger.h"
 
-void outside_addtoorders(int outsideorders[], int floor){
-    outsideorders[floor-1] = 1;
+void outside_addtoorders(int outsideorders[], int floor, ButtonType btn){
+    if(btn == 1){
+        outsideorders[floor*2]
+    }
+    return;
 }
 
 void outside_ordercomplete(int outsideorders[], int floor){
@@ -24,27 +27,76 @@ void inside_addtoorders(int insideorders[], int floor){
 }
 
 int getnextfloor(MotorDirection motordir, int currentfloor, int outsideorders[], int insideorders[]){
-    if(motordir = 1){}
-    int targetfloor;
-    int nextfloor;
+    int targetfloorout;
+    int nextfloorout, nextfloorin = 0;
+    int nextfloor = currentfloor;
+    int emptylistout[6];
+    int emptylistin[4];
 
+    // sjekk om listene er tomme
+    if(outsideorders == emptylistout && insideorders == emptylistin){return currentfloor;}
+
+    
+    // finn neste etasje fra knapp som trykkes utenfor heisen
     for(int i = 0 ; i<=5; i++) {
-        if(i == 0){targetfloor = 1;}
-        if(i == 1 || i == 2){targetfloor = 2;}
-        if(i == 3 || i == 4){targetfloor = 3;}
-        if(i == 5){targetfloor = 5;}
+        if(i == 0){targetfloorout = 1;}
+        if(i == 1 || i == 2){targetfloorout = 2;}
+        if(i == 3 || i == 4){targetfloorout = 3;}
+        if(i == 5){targetfloorout = 5;}
+        switch (motordir)
+        {
+        case -1: 
+            if(targetfloorout <= currentfloor){
+                if ((outsideorders[i] == 1) && (targetfloorout > nextfloorout)){nextfloorout = targetfloorout;}
+            }                    
+            break;
+        case 0:
+            if(targetfloorout == currentfloor){return currentfloor}
+            if(outsideorders[i] == 1){
+                if(abs(currentfloor - targetfloorout) <= abs(currentfloor - nextfloorout)){
+                    nextfloorout = targetfloor;
+                }
+            };            
+            break;
+        case 1:
+            if(targetfloorout >= currentfloor){
+                if ((outsideorders[i] == 1) && (targetfloorout < nextfloorout)){nextfloorout = targetfloorout;}
+            }            
+            break;
+        default:
+            break;
+        }      
+    }
 
-        if((motordir == 1 && targetfloor <= currentfloor) || (motordir == -1 && targetfloor >= currentfloor)){
-            continue;
-        if((motordir == 1 && targetfloor > currentfloor) || (motordir == -1 && targetfloor < currentfloor)){
-                if (outsideorders[i] == 1){nextfloor = targetfloor;}
-            } 
-        }       
+    //sjekker innsiden
+    for(int i = 1; i<=4; i++){
+    switch (motordir)
+    {
+    
+    case 1: 
+        if((insideorders[i] == 1) && (i < nextfloorin) && (i >= currentfloor)){nextfloorin = i;}
+        break;
+    case 0:
+        if(i == currentfloor){return currentfloor}
+        if((insideorders[i] == 1 && nextfloorin != currentfloor)){
+            if(abs(currentfloor-i) <= abs(currentfloor - nextfloorin)){nextfloorin = i;}
+        break;
+    case -1:
+        if((insideorders[i] == 1) && (i > nextfloorin) && (i <= currentfloor)){nextfloorin = i;}
+        break;
+    
+    default:
+        break;
+    }}
+
+
+    if(nextfloorout-nextfloorin <= 0){
+        nextfloor = nextfloorout;
     }
-    if(motordir == 1){
-        for(int i = 0; i <= 4; i++){
-            if((i > currentfloor) && (insideorders[i] == 1) && (i < nextfloor)){i = nextfloor;}
-        }
+    if(nextfloorout - nextfloorin > 0){
+        nextfloor = nextfloorin;
     }
+
+    return nextfloor;
 }
 
