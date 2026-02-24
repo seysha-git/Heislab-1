2eb9f0eced39627d9bad96aa46f86a94185fd10d
Simennha <simennha@stud.ntnu.no>
2026-02-24 15:28:35 +0100

    sag

--- lab_2-1.1.4/skeleton_project/source/lys.h
+++ lab_2-1.1.4/skeleton_project/source/lys.h
@@ -1,11 +1,11 @@
-#pragma once
-#include <stdbool.h>
-#include "driver/elevio.h"
+// #pragma once
+// #include <stdbool.h>
+// #include "driver/elevio.h"
 
-// Bestillingslys (L1/L2) fra ordrelisten:
-void lights_set_order_lamps_from_orders(void);
+// // Bestillingslys (L1/L2) fra ordrelisten:
+// void lights_set_order_lamps_from_orders(void);
 
-// Andre lys (om du vil samle dem her også):
-void lights_set_floor_indicator(int floor);     // L3-L5
-void lights_set_stop_lamp(bool on);             // L6
-void lights_set_door_open_lamp(bool on);        // ofte brukt
+// // Andre lys (om du vil samle dem her også):
+// void lights_set_floor_indicator(int floor);     // L3-L5
+// void lights_set_stop_lamp(bool on);             // L6
+// void lights_set_door_open_lamp(bool on);        // ofte brukt
